8caf9ae0230bf3e254a12ca5862b37398d350d92
Gopal Mathur <mathuyrgopal@gmail.com>
2026-02-28 19:15:49 +0530

    custom dataset for custom data

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,15 +1,18 @@
 import torch
-from torch.utils.data import DataLoader, Dataset
-from typing_extensions import Tuple
+from torch.utils.data import Dataset
 
 
 class CustomDataset(Dataset):
-    def __init__(self, inputs, targets) -> None:
-        self.inputs = torch.tensor(inputs, dtype=torch.long)
-        self.targets = torch.tensor(targets, dtype=torch.long)
+    def __init__(self, token_stream: torch.Tensor, seq_len: int) -> None:
+        if token_stream.dtype != torch.long or token_stream.dim() != 1:
+            raise ValueError("token_stream must be a 1D torch.long tensor of token ids")
+        self.data = token_stream
+        self.seq_len = seq_len
 
     def __len__(self) -> int:
-        return len(self.inputs)
+        return len(self.data) - self.seq_len - 1
 
-    def __getitem__(self, index) -> Tuple:
-        return self.inputs[index], self.targets[index]
+    def __getitem__(self, idx: int):
+        x = self.data[idx : idx + self.seq_len]  # (T,)
+        y = self.data[idx + 1 : idx + self.seq_len + 1]  # (T,)
+        return x, y
